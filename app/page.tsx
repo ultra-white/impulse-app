bdad9b587f9c4dbc7fd25cfe989ae5b143e5ffee
Obukhov Dmitry <lesnitca143@yandex.ru>
2025-03-19 18:46:58 +0300

    Refactor main section layout and improve block styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
 	title: "Импульс – Платформа для развития",
@@ -30,9 +31,9 @@ export default function Home() {
 	return (
 		<>
 			<header className='font-involve h-full bg-[#141414] text-white flex flex-col items-center md:px-[50px] px-6 md:rounded-b-[100px] rounded-b-[50px]'>
-				<a href='' className='mt-[30px]'>
+				<Link href='' className='mt-[30px]'>
 					<Image src='/logo.svg' alt='logo' width={274} height={48} />
-				</a>
+				</Link>
 
 				<div className='mt-12 w-full flex flex-col items-center relative'>
 					<Image
@@ -107,7 +108,7 @@ export default function Home() {
 
 					<div className='w-full grid xl:grid-cols-2 grid-cols-1 gap-6 mt-5 text-lg'>
 						<div className='w-full grid gap-6'>
-							<div className='md:col-span-2 p-3 rounded-4xl shadow-lg flex flex-col relative overflow-hidden md:md:h-[400px] h-[300px]'>
+							<div className='md:col-span-2 p-3 rounded-4xl shadow-lg flex flex-col relative overflow-hidden md:md:h-[400px] h-[300px] hover:scale-[101%] duration-300'>
 								<Image
 									src='/calculator.png'
 									alt='Calculator'
@@ -123,7 +124,7 @@ export default function Home() {
 									</p>
 								</div>
 							</div>
-							<div className='md:col-span-1 bg-black p-6 rounded-4xl shadow-lg flex flex-col md:h-[400px] h-[300px]'>
+							<div className='md:col-span-1 bg-black p-6 rounded-4xl shadow-lg flex flex-col md:h-[400px] h-[300px] hover:scale-[101%] duration-300'>
 								<div className='flex flex-col gap-[6px]'>
 									<h3 className='text-2xl font-[family-name:var(--font-gerhaus)] text-[#D8C5FF]'>
 										ПРОВЕРКА СВОИХ ВОЗМОЖНОСТЕЙ
@@ -134,7 +135,7 @@ export default function Home() {
 									</p>
 								</div>
 							</div>
-							<div className=' p-3 rounded-4xl shadow-lg flex flex-col justify-end bg-[#d8c5ff] md:h-[400px] h-[300px]'>
+							<div className=' p-3 rounded-4xl shadow-lg flex flex-col justify-end bg-[#d8c5ff] md:h-[400px] h-[300px] hover:scale-[101%] duration-300'>
 								<div className='bg-white rounded-xl p-3 flex flex-col gap-[6px]'>
 									<h3 className='text-2xl font-[family-name:var(--font-gerhaus)] text-black'>
 										ПРЕИМУЩЕСТВО НА РЫНКЕ ТРУДА
@@ -146,7 +147,7 @@ export default function Home() {
 							</div>
 						</div>
 						<div className='w-full grid gap-6'>
-							<div className='bg-[#141414] text-white p-6 rounded-4xl shadow-lg flex flex-col justify-end md:h-[400px] h-[300px]'>
+							<div className='bg-[#141414] text-white p-6 rounded-4xl shadow-lg flex flex-col justify-end md:h-[400px] h-[300px] hover:scale-[101%] duration-300'>
 								<div className='flex md:flex-col-reverse flex-col gap-[6px]'>
 									<h3 className='text-2xl font-[family-name:var(--font-gerhaus)] text-[#D8C5FF]'>
 										ШАНС ПОПАСТЬ В IT-ГИГАНТЫ
@@ -157,13 +158,13 @@ export default function Home() {
 									</p>
 								</div>
 							</div>
-							<div className='md:col-span-1 bg-[#d8c5ff] p-3 rounded-4xl shadow-lg flex flex-col md:h-[400px] h-[300px]'>
+							<div className='md:col-span-1 bg-[#d8c5ff] p-3 rounded-4xl shadow-lg flex flex-col md:h-[400px] h-[300px] hover:scale-[101%] duration-300'>
 								<div className='bg-white rounded-xl p-3 flex flex-col gap-[6px]'>
 									<h3 className='text-2xl font-[family-name:var(--font-gerhaus)]'>ПОЛЕЗНЫЕ ЗНАКОМСТВА</h3>
 									<p>на конкурсах ты найдёшь будущих коллег, единомышленников и партнёров для стартапов.</p>
 								</div>
 							</div>
-							<div className='md:col-span-2 p-3 rounded-4xl shadow-lg flex flex-col relative overflow-hidden md:h-[400px] h-[300px]'>
+							<div className='md:col-span-2 p-3 rounded-4xl shadow-lg flex flex-col relative overflow-hidden md:h-[400px] h-[300px] hover:scale-[101%] duration-300'>
 								<Image
 									src='/helmet.png'
 									alt='Helmet'
@@ -219,6 +220,224 @@ export default function Home() {
 									</p>
 								</div>
 							</div>
+						</div>
+					</div>
+				</section>
+				<section className='md:px-[50px] px-6 md:mt-[100px] mt-[75px]'>
+					<h2 className='font-[family-name:var(--font-gerhaus)] 2xl:text-[48px] md:text-4xl sm:text-3xl text-2xl'>
+						Данные в цифрах
+					</h2>
+					<div className='md:mt-[50px] mt-6 flex gap-4 text-center lg:flex-nowrap flex-wrap justify-center items-center'>
+						<div className='flex gap-4 justify-center items-center sm:flex-nowrap flex-wrap'>
+							<div>
+								<h3 className='font-[family-name:var(--font-gerhaus)] text-[85px]'>800K</h3>
+								<p>Открытых вакансий в области IT по данным Хабр.Карьера</p>
+							</div>
+							<div>
+								<h3 className='font-[family-name:var(--font-gerhaus)] text-[85px]'>50%</h3>
+								<p>участников хакатонов получают предложения о работе от компаний-спонсоров мероприятий.</p>
+							</div>
+						</div>
+						<div className='flex gap-4 justify-center items-center sm:flex-nowrap flex-wrap'>
+							<div>
+								<h3 className='font-[family-name:var(--font-gerhaus)] text-[85px]'>123к</h3>
+								<p>среднемесячная зарплата в области IT по данным Росстата</p>
+							</div>
+							<div>
+								<h3 className='font-[family-name:var(--font-gerhaus)] text-[85px]'>550</h3>
+								<p>Образовательных IT мероприятий проходят в России ежегодно</p>
+							</div>
+						</div>
+					</div>
+				</section>
+				<section className='md:px-[50px] px-6 md:mt-[120px] mt-[85px] mb-[100px]'>
+					<h2 className='font-[family-name:var(--font-gerhaus)] 2xl:text-[48px] md:text-4xl sm:text-3xl text-2xl'>
+						Выбирай в чём учавствовать
+					</h2>
+					<div className='md:mt-[50px] mt-6 flex flex-col gap-5'>
+						<div className='flex xl:flex-nowrap flex-wrap gap-5'>
+							<Link href='' className='bg-black rounded-4xl w-full h-full p-8 relative hover:scale-[101%] duration-300'>
+								<div className='text-white flex md:gap-8 items-start md:items-center flex-col md:flex-row'>
+									<h3 className='text-[40px] font-[family-name:var(--font-gerhaus)]'>IT-Планета</h3>
+									<span className='py-1 px-4 bg-zinc-600 rounded-2xl text-zinc-300'>чемпионат</span>
+								</div>
+								<Image
+									src='/arrow_link.png'
+									alt='link'
+									width={70}
+									height={70}
+									className='absolute md:top-6 md:right-6 top-1 right-1 md:w-[70px] w-[45px]'
+								/>
+								<div className='flex gap-2.5 md:mt-2.5 mt-4 flex-wrap'>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										Кибербезопасность
+									</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>Веб-разработка</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>Роботостроение</span>
+								</div>
+								<p className='flex gap-2.5 mt-4 text-white'>
+									<span className='text-zinc-400'>Для кого:</span> Студенты ВУЗов и СПО
+								</p>
+								<p className='mt-15 text-white'>
+									Участников хакатонов получают предложения о работе от компаний-спонсоров мероприятий.
+								</p>
+							</Link>
+							<Link href='' className='bg-black rounded-4xl w-full h-full p-8 relative hover:scale-[101%] duration-300'>
+								<div className='text-white flex md:gap-8 items-start md:items-center flex-col md:flex-row'>
+									<h3 className='text-[40px] font-[family-name:var(--font-gerhaus)]'>Музеи. Парки. Усадьбы</h3>
+									<span className='py-1 px-4 bg-zinc-600 rounded-2xl text-zinc-300'>олимпиада</span>
+								</div>
+								<Image
+									src='/arrow_link.png'
+									alt='link'
+									width={70}
+									height={70}
+									className='absolute md:top-6 md:right-6 top-1 right-1 md:w-[70px] w-[45px]'
+								/>
+								<div className='flex gap-2.5 md:mt-2.5 mt-4 flex-wrap'>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>История</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										Культура и природа столицы
+									</span>
+								</div>
+								<p className='flex gap-2.5 mt-4 text-white'>
+									<span className='text-zinc-400'>Для кого:</span> Студенты ВУЗов и СПО
+								</p>
+								<p className='mt-15 text-white'>История, культура и природа столицы.</p>
+							</Link>
+						</div>
+						<div className='flex xl:flex-nowrap flex-wrap gap-5'>
+							<Link href='' className='bg-black rounded-4xl w-full h-full p-8 relative hover:scale-[101%] duration-300'>
+								<div className='text-white flex md:gap-8 items-start md:items-center flex-col md:flex-row'>
+									<h3 className='text-[40px] font-[family-name:var(--font-gerhaus)]'>Абилимпикс</h3>
+									<span className='py-1 px-4 bg-zinc-600 rounded-2xl text-zinc-300'>чемпионат</span>
+								</div>
+								<Image
+									src='/arrow_link.png'
+									alt='link'
+									width={70}
+									height={70}
+									className='absolute md:top-6 md:right-6 top-1 right-1 md:w-[70px] w-[45px]'
+								/>
+								<div className='flex gap-2.5 md:mt-2.5 mt-4 flex-wrap'>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>IT</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										Кибербезопасность
+									</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										Графический дизайн
+									</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										Автоматизация бизнес-процессов
+									</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										Технологии моды
+									</span>
+								</div>
+								<p className='flex gap-2.5 mt-4 text-white'>
+									<span className='text-zinc-400'>Для кого:</span> Студенты ВУЗов и СПО
+								</p>
+								<p className='mt-15 text-white'></p>
+							</Link>
+						</div>
+						<div className='flex xl:flex-nowrap flex-wrap gap-5'>
+							<Link href='' className='bg-black rounded-4xl w-full h-full p-8 relative hover:scale-[101%] duration-300'>
+								<div className='text-white flex md:gap-8 items-start md:items-center flex-col md:flex-row'>
+									<h3 className='text-[40px] font-[family-name:var(--font-gerhaus)]'>Не прервётся связь поколений</h3>
+									<span className='py-1 px-4 bg-zinc-600 rounded-2xl text-zinc-300'>олимпиада</span>
+								</div>
+								<Image
+									src='/arrow_link.png'
+									alt='link'
+									width={70}
+									height={70}
+									className='absolute md:top-6 md:right-6 top-1 right-1 md:w-[70px] w-[45px]'
+								/>
+								<div className='flex gap-2.5 md:mt-2.5 mt-4 flex-wrap'>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										Кибербезопасность
+									</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>Истории</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>Культура</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										Семейныу традиции
+									</span>
+								</div>
+								<p className='flex gap-2.5 mt-4 text-white'>
+									<span className='text-zinc-400'>Для кого:</span> Школьники, студенты СПО
+								</p>
+								<p className='mt-15 text-white'>
+									Метапредметная олимпиада для школьников, посвящённая истории, культуре и семейным традициям.
+								</p>
+							</Link>
+							<Link href='' className='bg-black rounded-4xl w-full h-full p-8 relative hover:scale-[101%] duration-300'>
+								<div className='text-white flex md:gap-8 items-start md:items-center flex-col md:flex-row'>
+									<h3 className='text-[40px] font-[family-name:var(--font-gerhaus)]'>МоиФинансы</h3>
+									<span className='py-1 px-4 bg-zinc-600 rounded-2xl text-zinc-300'>олимпиада</span>
+								</div>
+								<Image
+									src='/arrow_link.png'
+									alt='link'
+									width={70}
+									height={70}
+									className='absolute md:top-6 md:right-6 top-1 right-1 md:w-[70px] w-[45px]'
+								/>
+								<div className='flex gap-2.5 md:mt-2.5 mt-4 flex-wrap'>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										Кибербезопасность
+									</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>Личные финансы</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>Бюджетирование</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>Инвестиции</span>
+								</div>
+								<p className='flex gap-2.5 mt-4 text-white'>
+									<span className='text-zinc-400'>Для кого:</span> Студенты ВУЗов и СПО
+								</p>
+								<p className='mt-15 text-white'>Всероссийская олимпиада по финансовой грамотности для студентов СПО.</p>
+							</Link>
+						</div>
+						<div className='flex xl:flex-nowrap flex-wrap gap-5'>
+							<Link href='' className='bg-black rounded-4xl w-full h-full p-8 relative hover:scale-[101%] duration-300'>
+								<div className='text-white flex md:gap-8 items-start md:items-center flex-col md:flex-row'>
+									<h3 className='text-[40px] font-[family-name:var(--font-gerhaus)]'>Московские мастера</h3>
+									<span className='py-1 px-4 bg-zinc-600 rounded-2xl text-zinc-300'>конкурс</span>
+								</div>
+								<Image
+									src='/arrow_link.png'
+									alt='link'
+									width={70}
+									height={70}
+									className='absolute md:top-6 md:right-6 top-1 right-1 md:w-[70px] w-[45px]'
+								/>
+								<div className='flex gap-2.5 md:mt-2.5 mt-4 flex-wrap'>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										Кибербезопасность
+									</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										3D-моделирование
+									</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										Графический дизайн
+									</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										Интернет-маркетинг
+									</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>Робототехника</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										Сетевое администрирование
+									</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>
+										Программные решения для бизнеса
+									</span>
+									<span className='border rounded-2xl border-[#D8C5FF] text-[#D8C5FF] py-1 px-2.5'>Веб-технологии</span>
+								</div>
+								<p className='flex gap-2.5 mt-4 text-white'>
+									<span className='text-zinc-400'>Для кого:</span> Студенты ВУЗов и СПО
+								</p>
+								<p className='mt-15 text-white'>
+									Конкурс профессионального мастерства для студентов колледжей и школьников Москвы.
+								</p>
+							</Link>
 						</div>
 					</div>
 				</section>
